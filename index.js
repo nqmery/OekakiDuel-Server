f149e28d7d8865c5b2d74c9f4e37213c302b8bd7
carbood <168190155+carbood@users.noreply.github.com>
2024-12-17 14:17:39 +0900

    特殊効果の処理を少し追加

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = 3000;
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
-cards1 = Card[5];  //プレイヤー１のカード合計枚数
-cards2 = Card[5];  //プレイヤー２のカード合計枚数
+cards = Card[10];  //プレイヤー１のカード合計枚数
 roundnum = 0;      // 現在ラウンド数
 
 //あとでインスタンス化
@@ -52,8 +51,8 @@ wss.on('connection', function(ws, req) {
       console.log("string received from client -> '" + data + "'");
       ws.send("[Server]string received from client -> '" + data + "'");
 
-      SelectCard1 = new Card(1,1,50,50,50,50); //クライアント１が選択したカード
-      SelectCard2 = new Card(1,2,23,23,23,23); //クライアント２が選択したカード、どうやってクライアントを区別するんだっけ
+      cards[0] = new Card(1,1,50,50,50,50); //クライアント１が選択したカード
+      cards[1] = new Card(1,2,23,23,23,23); //クライアント２が選択したカード、どうやってクライアントを区別するんだっけ
 
     } else {
       console.log("binary received from client -> " + Array.from(data).join(", ") + "");
@@ -78,22 +77,50 @@ wss.on('connection', function(ws, req) {
         }
       }
     }
-
+    
+    if(byte[0] === 32 && roundnum <= 5){
+    //特殊効果発動順序
+    if(SelectCard1.spd > SelectCard2.spd){
+      if(byte[5] === 0){
+        BattleFlow();
+      }
+      //確実に先制攻撃
+      if(byte[5] === 1){
+        if(SelectCard1.player === byte[3]){
+          SelectCard2.spd = 0;
+        }else{
+          SelectCard1.spd = 0;
+        }
+      }
+      //相手の攻撃無効化
+      if(byte[5] === 2){
+        if(SelectCard1.player === byte[3]){
+          SelectCard2.atk = 0;
+        }else{
+          SelectCard1.atk = 0;
+        }
+      }
+      
+    }
+    
+      function  BattleFlow(){
       //カードの速さを比較
       if(SelectCard1.spd > SelectCard2.spd){
         //先にプレイヤー１が攻撃
-        BattleFlow(SelectCard2.player, SelectCard1.atk, SelectCard2.def, Player2.hp); 
+        BattleCalc(SelectCard2.player, SelectCard1.atk, SelectCard2.def, Player2.hp); 
         //後からプレイヤー２が攻撃
-        BattleFlow(SelectCard1.player, SelectCard2.atk, SelectCard1.def, Player1.hp);
+        BattleCalc(SelectCard1.player, SelectCard2.atk, SelectCard1.def, Player1.hp);
       } else {
         //先にプレイヤー２が攻撃
-        BattleFlow(SelectCard1.player, SelectCard2.atk, SelectCard1.def, Player1.hp);
+        BattleCalc(SelectCard1.player, SelectCard2.atk, SelectCard1.def, Player1.hp);
         //後からプレイヤー１が攻撃
-        BattleFlow(SelectCard2.player, SelectCard1.atk, SelectCard2.def, Player2.hp);
+        BattleCalc(SelectCard2.player, SelectCard1.atk, SelectCard2.def, Player2.hp);
       }
+    }
+    }
 
-      // バトル中の処理
-      function BattleFlow(playernum, atknum, defnum, hpnum){
+      // バトル中のダメージ計算
+      function BattleCalc(playernum, atknum, defnum, hpnum){
         
         //負の値にならないようにして計算結果を定義
         Damagevalue = Math.max(defnum - atknum, 0); 
